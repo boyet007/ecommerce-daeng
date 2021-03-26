470de2e93bfb64c8b547f2738feadf485d1a6403
boyet <boyet007@gmail.com>
2021-03-26 16:45:37 +0700

    fix bug

--- resources/views/report/return.blade.php
+++ resources/views/report/return.blade.php
@@ -1,52 +1,99 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Return PDF</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-</head>
-<body>
-    <h5>Laporan Return Periode ({{ $date[0] }} - {{ $date[1] }})</h5>
-    <hr>
-    <table width="100%" class="table-hover table-bordered">
-        <thead>
-            <tr>
-                <th>InvoiceID</th>
-                <th>Pelanggan</th>
-                <th>Subtotal</th>
-                <th>Tanggal</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php $total = 0; @endphp
-            @forelse ($orders as $row)
-                <tr>
-                    <td><strong>{{ $row->invoice }}</strong></td>
-                    <td>
-                        <strong>{{ $row->customer_name }}</strong><br>
-                        <label><strong>Telp:</strong> {{ $row->customer_phone }}</label><br>
-                        <label><strong>Alamat:</strong> {{ $row->customer_address }} {{ $row->customer->district->name }} - {{  $row->customer->district->city->name}}, {{ $row->customer->district->city->province->name }}</label>
-                    </td>
-                    <td>Rp {{ number_format($row->subtotal) }}</td>
-                    <td>{{ $row->created_at->format('d-m-Y') }}</td>
-                </tr>
+@extends('layouts.admin')
 
-                @php $total += $row->subtotal @endphp
-            @empty
-            <tr>
-                <td colspan="6" class="text-center">Tidak ada data</td>
-            </tr>
-            @endforelse
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="2">Total</td>
-                <td>Rp {{ number_format($total) }}</td>
-                <td></td>
-            </tr>
-        </tfoot>
-    </table>
-</body>
-</html>
+@section('title')
+    <title>Laporan Return</title>
+@endsection
+
+@section('content')
+<main class="main">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item">Home</li>
+        <li class="breadcrumb-item active">Laporan Return</li>
+    </ol>
+    <div class="container-fluid">
+        <div class="animated fadeIn">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">
+                                Laporan Return
+                            </h4>
+                        </div>
+                        <div class="card-body">
+                            @if (session('success'))
+                                <div class="alert alert-success">{{ session('success') }}</div>
+                            @endif
+
+                            @if (session('error'))
+                                <div class="alert alert-danger">{{ session('error') }}</div>
+                            @endif
+
+                            <form action="{{ route('report.return') }}" method="get">
+                                <div class="input-group mb-3 col-md-4 float-right">
+                                    <input type="text" id="created_at" name="date" class="form-control">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-secondary" type="submit">Filter</button>
+                                    </div>
+                                    <a target="_blank" class="btn btn-primary ml-2" id="exportpdf">Export PDF</a>
+                                </div>
+                            </form>
+                            <div class="table-responsive">
+                                <table class="table table-hover table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>InvoiceID</th>
+                                            <th>Pelanggan</th>
+                                            <th>Subtotal</th>
+                                            <th>Tanggal</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @forelse ($orders as $row)
+                                        <tr>
+                                            <td><strong>{{ $row->invoice }}</strong></td>
+                                            <td>
+                                                <strong>{{ $row->customer_name }}</strong><br>
+                                                <label><strong>Telp:</strong> {{ $row->customer_phone }}</label><br>
+                                                <label><strong>Alamat:</strong> {{ $row->customer_address }} {{ $row->customer->district->name }} - {{  $row->customer->district->city->name}}, {{ $row->customer->district->city->province->name }}</label>
+                                            </td>
+                                            <td>Rp {{ number_format($row->subtotal) }}</td>
+                                            <td>{{ $row->created_at->format('d-m-Y') }}</td>
+                                        </tr>
+                                        @empty
+                                        <tr>
+                                            <td colspan="6" class="text-center">Tidak ada data</td>
+                                        </tr>
+                                        @endforelse
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+@endsection
+
+@section('js')
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/momentjs/latest/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" />
+    <script>
+        $(document).ready(function() {
+            let start = moment().startOf('month')
+            let end = moment().endOf('month')
+
+            $('#exportpdf').attr('href', '/administrator/reports/return/pdf/' + start.format('YYYY-MM-DD') + '+' + end.format('YYYY-MM-DD'))
+
+            $('#created_at').daterangepicker({
+                startDate: start,
+                endDate: end
+            }, function(first, last) {
+                $('#exportpdf').attr('href', '/administrator/reports/return/pdf/' + first.format('YYYY-MM-DD') + '+' + last.format('YYYY-MM-DD'))
+            })
+        })
+    </script>
+@endsection()
